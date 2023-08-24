a9abf5d9fe3348d2fea27b3268aa9810d04a717e
gabriel073 <gabrielmarzioli@hotmail.com>
2023-08-24 17:06:45 -0300

    creando variables de entorno

--- src/Contacto.jsx
+++ src/Contacto.jsx
@@ -1,31 +1,56 @@
-// import { Button, Input, InputGroup, Stack, Text, Textarea } from '@chakra-ui/react';
-import { Button, Input, Stack, Text, Textarea } from '@chakra-ui/react';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { Input, Stack, Text, Textarea } from '@chakra-ui/react';
 
-
-
+const { VITE_SERVICE_ID, VITE_TEMPLATE_ID, VITE_PUBLIC_KEY } = import.meta.env;
 
 export default function Contacto() {
 
-
+    const form = useRef();
     const [nameError, setNameError] = useState(false);
     const [emailError, setEmailError] = useState(false);
     const [messageError, setMessageError] = useState(false);
 
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: ''
-    });
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        setNameError(false);
+        setEmailError(false);
+        setMessageError(false);
+        if (!form.current.name) {
+            return setNameError("Este campo no puede estar vacio, favor de ingresar un nombre");
+        }
+        if (!form.current.name.value.split("").every(char => isNaN(parseInt(char)))) {
+            return setNameError("Su nombre no puede contener numeros");
+        }
+        if (!form.current.name) {
+            return setEmailError("Este campo no puede estar vacio, favor de ingresar un Mail");
+        }
+        if (!paternEmail.test(form.current.email.value)) {
+            return setEmailError("No es un formato de Mail válido");
+        }
+        if (!form.current.message) {
+            return setMessageError("Este campo no puede estar vacio, favor de ingresar un Mensaje");
+        }
+
+
+        emailjs.sendForm(VITE_SERVICE_ID, VITE_TEMPLATE_ID, form.current, VITE_PUBLIC_KEY)
+            .then((result) => {
+                console.log('VARIABLE ENTORNO', VITE_SERVICE_ID);
+                console.log(result.text);
+                form.current.reset();
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
 
     const handleInputChange = event => {
         const { name, value } = event.target;
         setNameError(false);
         setEmailError(false);
         setMessageError(false);
-
-        setFormData({
-            ...formData,
+        form({
+            ...form,
             [name]: value
         });
     }
@@ -33,98 +58,59 @@ export default function Contacto() {
     const paternEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        // Aquí puedes realizar acciones con los datos del formulario, como enviarlos a un servidor
-        setNameError(false);
-        setEmailError(false);
-        setMessageError(false);
-        if (!formData.name) {
-            return setNameError("Este campo no puede estar vacio, favor de ingresar un nombre");
-        }
-        if (!formData.name.split("").every(char => isNaN(parseInt(char)))) {
-            return setNameError("Su nombre no puede contener numeros");
-        }
-        if (!formData.email) {
-            return setEmailError("Este campo no puede estar vacio, favor de ingresar un Mail");
-        }
-        if (!paternEmail.test(formData.email)) {
-            return setEmailError("No es un formato de Mail válido");
-        }
-        if (!formData.message) {
-            return setMessageError("Este campo no puede estar vacio, favor de ingresar un Mensaje");
-        }
-        alert(`Datos Enviados con exito!!! <br/> ${formData.name}, ${formData.email}, ${formData.message}`);
-
-        setFormData({
-            name: "",
-            email: "",
-            message: "",
-        }
-        )
-
-    }
-
     return (
         <Stack mb={150} m='auto' h={620} w={500} alignItems='center' spacing={3} borderRadius={40}
             border='solid white 2px'>
             <div id="contacto">
                 <h1>Contacto</h1>
-                <form action="https://formcarry.com/s/cB7cA0IiQf" method="POST" onSubmit={handleSubmit} >
 
-                    <label htmlFor="nombre">Nombre:</label>
+                <form ref={form} onSubmit={sendEmail}>
+                    <label>Nombre</label>
                     <Input
                         h={25}
                         type="text"
                         width='100%'
-                        mb='60px'
+                        mb='0px'
                         id="name"
-                        name="name"
-                        value={formData.name}
+                        name="user_name"
+                        // value={formData.name}
                         onChange={handleInputChange}
                         required
                     />
-                    <Stack color='red' mb='-50px' mt='-40px'>
+                    <Stack color='red' mb='0px' mt='0px'>
                         {nameError && <p>{nameError}</p>}
                     </Stack>
-                    <br /><br />
-
-                    <label htmlFor="email">Email:</label>
+                    <label>Email</label>
                     <Input
                         h={25}
                         type="email"
                         id="email"
-                        name="email"
-                        mb='60px'
+                        name="user_email"
+                        mb='10px'
                         width='100%'
-                        value={formData.email}
+                        // value={formData.email}
                         onChange={handleInputChange}
-                        required
+
                     />
-                    <Stack color='red' mb='-50px' mt='-40px'>
+                    <Stack color='red' mb='0px' mt='0px'>
                         {emailError && <p>{emailError}</p>}
                     </Stack>
-                    <br /><br />
-
-
-                    <label htmlFor="mensaje">Mensaje:</label><br />
+                    <label>Mensaje</label>
                     <Textarea
-                        mb='50px'
+                        mb='40px'
                         id="message"
                         name="message"
                         rows="8"
                         cols="50"
                         width='100%'
-                        value={formData.message}
+                        // value={formData.message}
                         onChange={handleInputChange}
-                        required
+
                     ></Textarea>
-                    <Stack color='red' mb='-50px' mt='-50px'>
+                    <Stack color='red' mb='0px' mt='0px'>
                         {messageError && <p>{messageError}</p>}
                     </Stack>
-                    <br /><br />
-
-                    <Button type="submit" onClick={handleSubmit} backgroundColor='blue' mt={20} width='80%' h='40px' >Enviar</Button>
+                    <Input type="submit" value="Send" backgroundColor='blue' mt={20} width='80%' h='40px' />
                 </form>
             </div>
             <Stack mt='2%'>
@@ -133,4 +119,5 @@ export default function Contacto() {
         </Stack >
     );
 }
+
 
